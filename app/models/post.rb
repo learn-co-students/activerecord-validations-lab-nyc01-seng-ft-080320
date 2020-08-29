d166caef77e04e0bc50b3a9513585e60e55c87e3
royFloresNyc <royfloresnyc@gmail.com>
2020-08-29 17:03:49 -0400

    Complete Lab

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-fiction),
+        message: "%{value} is not a valid category" }
+    validate :title_is_clickbait 
+
+    
+
+    def title_is_clickbait
+        click_bait_patterns = [/Won't Believe/, /Secret/, /Top [0-9]/, /Guess/]
+        if click_bait_patterns.none? {|pattern| pattern.match title }
+            errors.add(:title, "must be clickbait")
+        end
+    end 
+
 end
